3589076dca0eaf3591a48f59a3f8d31ea6d5fd5e
Grace <gracejiayizhang@gmail.com>
2025-07-12 18:00:45 -0400

    basic setup

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+
+// Loads the index.html web page into a BrowserWindow instance
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 300,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  
+  // On macOS, recreate a window when the app is activated (e.x. clicking the dock icon)
+  // and there are no other open windows
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit the application when all windows are closed, except on macOS
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
